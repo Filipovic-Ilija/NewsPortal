32e5bd78f7ec3738f4f5f397c44f9379eeffc8fd
Filipovic <filipovic.official@gmail.com>
2022-01-31 20:30:25 +0100

    News Commit

---
+++ app/Http/Controllers/Frontend/ExtraController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Session;
+
+class ExtraController extends Controller
+{
+    public function Hindi(){
+        Session::get('lang');
+        Session()->forget('lang');
+        Session()->put('lang','hindi');
+        return redirect()->back();
+    }
+
+    public function English(){
+        Session::get('lang');
+        Session()->forget('lang');
+        Session()->put('lang','english');
+        return redirect()->back();
+    }
+
+}
